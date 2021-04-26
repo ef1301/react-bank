3ff8efebd92382500c0c31465e2ef228d4847f74
Jessica Wong <39626651+wongjessica@users.noreply.github.com>
2021-04-25 20:46:07 -0400

    added bootstrap packages and li card elements for debit+credit pages

--- src/cards/Debit.jsx
+++ src/cards/Debit.jsx
@@ -1,9 +1,19 @@
 import React from "react";
+import Card from 'react-bootstrap/Card';
+import 'bootstrap/dist/css/bootstrap.min.css';
 
 const DebitCard = ({ debit }) => {
   return (
     <div id="debit-card">
-      {debit.description} {debit.amount} {debit.date}
+    <Card style={{ width: "40rem" }}>
+      <Card.Header>
+      <b>Debit Information for: </b> {debit.description}
+      </Card.Header>
+      <Card.Body>
+        <li><i>Debit Amount:</i> {debit.amount}</li>
+        <li><i>Debit Date:</i> {debit.date}</li>
+      </Card.Body>
+    </Card>
     </div>
   );
 };
